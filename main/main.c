a04bd37a77516616be38be3b869423cba696889a
Gabe Kaufman (WSL) <gkaufman93@gmail.com>
2024-04-12 16:25:25 -0700

    Copied over version of basic app for EPD from example

--- main/main.c
+++ main/main.c
@@ -5,8 +5,35 @@
 #include "freertos/task.h"
 #include "driver/timer.h"
 
+#define COLORED 	 (0)
+#define UNCOLORED (1)
 
 void app_main(void)
 {
+	Epd epd;
+	
+	// Instantiate e-paper frame_
+	unsigned char* frame_ = (unsigned char*)malloc(epd.width * epd.height / 8);
+	
+	Paint paint(frame_, epd.width, epd.height);
+	
+	// Instantiate e-paper display and clear pixels
+	ESP_LOGI("EPD", "e-Paper init and clear");
+  epd.LDirInit();
+  epd.Clear();
+
+  while (1) {
+		int d = 3;
+    for (char i = '0'; i <= '9'; i++)
+    {
+      // paint_.Clear(UNCOLORED);
+      paint_.DrawCharAt(d, d, i, &Font20, COLORED);
+      epd.DisplayPart(frame_);
+      d = d + 20;
+      vTaskDelay(500);
+    }
+	}
+	
+	esp_restart();
 
 }
