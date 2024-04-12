3d5c22e47dd52bf2d7fd5ed3ada4f99e4364aee0
Gabe Kaufman (WSL) <gkaufman93@gmail.com>
2024-04-12 15:02:45 -0700

    Initialized project

---
+++ main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "epaper.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/timer.h"
+
+
+void app_main(void)
+{
+
+}
