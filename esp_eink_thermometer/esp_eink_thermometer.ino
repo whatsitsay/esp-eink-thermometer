10abc4b80d9228c9432a38c4a1366e1096dc8adb
Gabriel Kaufman (WSL) <gkaufman93@gmail.com>
2024-05-12 17:53:45 -0700

    Fixed pinout and model of display
    Actually 1in54B, the red tricolor version. Successfully able to display numbers along diagonal, though as time goes on the red seems to leak through. May need to be cleared/specified as well.

--- esp_eink_thermometer/esp_eink_thermometer.ino
+++ esp_eink_thermometer/esp_eink_thermometer.ino
@@ -1,12 +1,17 @@
 #include <SPI.h>
-#include <EPD1in54.h>
+#include <EPD1in54B.h>
 #include <EPDPaint.h>
 
 #define COLORED   (0)
 #define UNCOLORED (1)
 
+#define EPD_MOSI  (33)
+#define EPD_SCK   (32)
+#define EPD_MISO  (23) // Not actually used
+#define EPD_CS    (14)
+
 EPDPaint* paint_;
-EPD1in54* epd;
+EPD1in54B* epd;
 unsigned char* frame_;
 char strBuf[50];
 
@@ -15,26 +20,20 @@ void setup() {
   Serial.println("Simple counting test for Waveshare 1.54\" display");
   Serial.println(strBuf);
   // Instantiate EPD
-  epd = new EPD1in54(26,27,15,25); // RST, DC, CS, BSY
-  // Restart SPI bus with new pins
-  SPI.begin(14, MISO, 13, SS);
+  epd = new EPD1in54B(26,27, EPD_CS, 25); // RST, DC, CS, BSY
+  // Change SPI pins
+  SPI.begin(EPD_SCK, EPD_MISO, EPD_MOSI, EPD_CS);
 
   // Instantiate painter
-	frame_ = (unsigned char*)malloc((epd->width * epd->height) / 8);
+	frame_ = (unsigned char*)calloc((epd->width * epd->height) / 8, sizeof(unsigned char)); // Zero-out
 	paint_ = new EPDPaint(frame_, epd->width, epd->height);
 
   // Clear pixels
   Serial.println("e-Paper init & clear");
-  if (epd->init(lutFullUpdate) != 0) {
+  if (epd->init() != 0) {
     Serial.print("Failed to init ePaper");
     return;
   }
-
-  // Clear code from EPD1in54ShowRunningTime.ino example
-  epd->clearFrameMemory(0xFF);   // bit set = white, bit reset = black
-  epd->displayFrame();
-  epd->clearFrameMemory(0xFF);   // bit set = white, bit reset = black
-  epd->displayFrame();
 
   Serial.println("Frame cleared! Starting loop");
 }
@@ -43,12 +42,11 @@ void loop() {
   int d = 3;
   for (char i = '0'; i <= '9'; i++)
   {
-    // paint_.Clear(UNCOLORED);
+    paint_->clear(UNCOLORED);
     sprintf(strBuf, "Drawing \#%d at (%d, %d)", i, d, d);
     Serial.println(strBuf);
     paint_->drawCharAt(d, d, i, &Font20, COLORED);
-    epd->setFrameMemory(frame_);
-    epd->displayFrame();
+    epd->displayFrame(frame_, NULL);
     d = d + 20;
     delay(1000);
   }
